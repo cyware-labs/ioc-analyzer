e77e02109f0b99c0b0ff220ab73916f9110a51cd
Neel Parmar <neel@cyware.com>
2019-04-24 23:15:26 +0530

    ioc_analyzer utility

--- ioc_analyzer/ioc_factory.py
+++ ioc_analyzer/ioc_factory.py
@@ -2,6 +2,7 @@ from ioc_analyzer.py_whois.whois import get_whois_data
 from ioc_analyzer.shodan.shodan import get_shodan_data
 from ioc_analyzer.virus_total.virus_total import get_virus_total_data
 
+
 class IOCFactory(object):
 
     def __init__(self, args):
@@ -12,7 +13,6 @@ class IOCFactory(object):
             ('whois', True): get_whois_data,
             ('shodan', True): get_shodan_data,
             ('virus_total', True): get_virus_total_data,
-            ('all', True): True
         }
         return command_mapping
 
@@ -21,8 +21,17 @@ class IOCFactory(object):
         result = {}
         command_mapping = self.get_command_mapping()
         for attr in args_attr:
-            if args_attr[attr] is True:
+            if attr != 'all' and args_attr[attr] is True:
                 result[attr] = command_mapping.get((attr, args_attr[attr]))(self.args)
+
+        all_flag = True
+        for attr in args_attr:
+            if args_attr[attr] is True:
+                all_flag = False
+
+        if args_attr['all'] is True or all_flag is True:
+            for command in command_mapping:
+                result[command[0]] = command_mapping[command](self.args)
 
         return result
 
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup, find_packages
 
+requirement = [
+    '''
+    certifi==2019.3.9
+    chardet==3.0.4
+    FirstCommand==0.1
+    idna==2.8
+    ipaddr==2.2.0
+    requests==2.21.0
+    urllib3==1.24.2
+    '''
+]
+
 setup(
     name="ioc_analyzer",
     version="0.1",
     packages=find_packages(),
     entry_points={
         'console_scripts': ['ioc_analyzer=ioc_analyzer.ioc_analyzer:ioc_analyzer']
-    }
+    },
+    install_requires=requirement
 )
